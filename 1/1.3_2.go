0822bcd8da2a7299112d57e00af93fa03bde3270
MAKSIM-PC\Maksim <maksimboikodev@gmail.com>
2022-08-03 08:56:11 +0300

    ex1.5

--- 1/1.3_2.go
+++ 1/1.3_2.go
@@ -1,1 +1,12 @@
-package _
+//Напишите программу, которая выводит "I like Go!"
+//Sample Input:
+//Sample Output:
+//	I like Go!
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("I like Go!")
+}
--- 1/1.3_3.go
+++ 1/1.3_3.go
@@ -1,1 +1,17 @@
+//Напишите программу, которая выведет "I like Go!" 3 раза.
+//Sample Input:
+
+//Sample Output:
+/*I like Go!
+I like Go!
+I like Go!*/
+
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("I like Go!")
+	fmt.Println("I like Go!")
+	fmt.Println("I like Go!")
+}
