26906d41a4d99e8a1a9801f6489003a271424929
MAKSIM-PC\Maksim <maksimboikodev@gmail.com>
2022-10-14 07:38:16 +0300

    ex2.5_9

--- 2/2.5_9.go
+++ 2/2.5_9.go
@@ -1,1 +1,27 @@
+/*
+Даются две строки X и S. Нужно найти и вывести первое вхождение подстроки S в строке X. Если подстроки S нет в строке X - вывести -1
+
+Sample Input:
+awesome
+es
+Sample Output:
+2
+*/
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var x, s string
+	fmt.Scan(&x, &s)
+	res := strings.Index(x, s)
+	if res >= 0 {
+		fmt.Print(res)
+	} else {
+		fmt.Print("-1")
+	}
+
+}
