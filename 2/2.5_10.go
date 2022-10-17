5f013036f38f8450cbdf28aa22c030f53562f303
MAKSIM-PC\Maksim <maksimboikodev@gmail.com>
2022-10-17 09:49:55 +0300

    ex2.5_10

--- 2/2.5_10.go
+++ 2/2.5_10.go
@@ -1,1 +1,23 @@
+/*
+ На вход дается строка, из нее нужно сделать другую строку, оставив только нечетные символы (считая с нуля)
+
+Sample Input:
+ihgewlqlkot
+Sample Output:
+hello
+*/
 package main
+
+import "fmt"
+
+func main() {
+	var str, b string
+	fmt.Scan(&str)
+	a := []rune(str)
+	for i := range a {
+		if i%2 != 0 {
+			b += string(a[i])
+		}
+	}
+	fmt.Print(string(b))
+}
