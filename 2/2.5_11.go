80e717ee6729e544d2d79ba2d553e3cd694ff9db
MAKSIM-PC\Maksim <maksimboikodev@gmail.com>
2022-10-18 07:43:48 +0300

    ex2.5_11

--- 2/2.5_11.go
+++ 2/2.5_11.go
@@ -1,1 +1,24 @@
+/*
+Дается строка. Нужно удалить все символы, которые встречаются более одного раза и вывести получившуюся строку
+
+Sample Input:
+zaabcbd
+Sample Output:
+zcd
+*/
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var str string
+	fmt.Scan(&str)
+	for i := range str {
+		if strings.Count(str, string(str[i])) == 1 {
+			fmt.Print(string(str[i]))
+		}
+	}
+}
